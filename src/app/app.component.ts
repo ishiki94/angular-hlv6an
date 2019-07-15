c32392089fd285d71da67068cfb543d2e9dbec3e
ishiki94 <52930051+ishiki94@users.noreply.github.com>
2019-07-15 11:16:26 -0600

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
+declare var $:any;
+
 @Component({
-  selector: 'my-app',
+  selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
-  name = 'Angular';
-}
+
+export class AppComponent{}
